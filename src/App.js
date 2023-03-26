b105b31311609aa282a9c10d049a32754ad9a2a1
julia <julichis@mail.ru>
2023-03-26 20:41:48 +0400

    basic html structure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import './App.scss';
 
 function App() {
   return (
-    <div>
-    
+    <div className='feed'>
+      <header className='feed__header'>
+        <div className="feed__left">
+          <h1 className='feed__title'>Notifications</h1>
+          <div className="feed__number">3</div>
+        </div>
+        <button className="feed__right feed__button">Mark all as read</button>
+      </header>
+      <main>
+        <ul className='feed__list'>
+          <li className='feed__list-item'>
+            <article className='notification'>
+              <img src="" alt="" className='notification__avatar'/>
+              <div className="notification__text">
+                <div className="notification__info">
+                  <div className="notification__username"></div>
+                  <div className="notification__action"></div>
+                  <div className="notification__subject"></div>
+                  <div className="notification__flag"></div>
+                </div>
+                <div className="notification__time"></div>
+              </div>
+            </article>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
