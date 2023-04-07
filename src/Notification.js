65560acf905026b146e07dac86ff477c76827831
julia <julichis@mail.ru>
2023-04-07 16:01:14 +0400

    fixed link href

--- src/Notification.js
+++ src/Notification.js
@@ -11,13 +11,13 @@ const Notification = ({avatar, username, action, subject, message, img, time, ty
         <img src={process.env.PUBLIC_URL + avatar} alt="" className='notification__avatar'/>
         <div className="notification__text">
           <div className="notification__info">
-            <span className="notification__username"><a href="">{username}</a></span>
+            <span className="notification__username"><a href="/">{username}</a></span>
             <span className="notification__action">{action}</span>
-            <span className="notification__subject"><a href="">{subject}</a></span>
+            <span className="notification__subject"><a href="/">{subject}</a></span>
             <span className="notification__flag">&#8226;</span>
           </div>
           <div className="notification__time">{time} ago</div>
-          {(type === "message") && (<div className="notification__message"><a href="">{message}</a></div>)}
+          {(type === "message") && (<div className="notification__message"><a href="/">{message}</a></div>)}
         </div>
       </div>
       {(type === "picture") && (<a href='' className="notification__img"><img src={img} alt="" /></a>)}
